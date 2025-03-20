9d0cb96b0a2f39531e4fe39845d326dfd9903e42
alamos.developer <alamos.developer@gmail.com>
2025-03-20 18:27:38 +0000

    agregando supabase al proyecto

--- obj/Debug/net8.0/myapp.AssemblyInfo.cs
+++ obj/Debug/net8.0/myapp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("myapp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0916f13a8e5688fa1938756dcbc4ab630d769739")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8fa36eed2e7ecdb6e04aeba8a1a0e124b1f00cb5")]
 [assembly: System.Reflection.AssemblyProductAttribute("myapp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("myapp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
