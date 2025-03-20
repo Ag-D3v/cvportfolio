cd07122e22f5dcf5aa0a3da06fd0ddd9ed0d4bb3
alamos.developer <alamos.developer@gmail.com>
2025-03-20 19:56:41 +0000

    agregando gitignore

--- obj/Debug/net8.0/myapp.AssemblyInfo.cs
+++ obj/Debug/net8.0/myapp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("myapp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a7aee9ab59ca74a3df70e334a9b854a8deb7d4e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26e212b376e1978baa8f515f14daadea311660cc")]
 [assembly: System.Reflection.AssemblyProductAttribute("myapp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("myapp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
