a69cfd498de11c84115b87c47cc2a8c023f3a7bd
alamos.developer <alamos.developer@gmail.com>
2025-03-20 19:56:57 +0000

    .

--- obj/Debug/net8.0/myapp.AssemblyInfo.cs
+++ obj/Debug/net8.0/myapp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("myapp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26e212b376e1978baa8f515f14daadea311660cc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd07122e22f5dcf5aa0a3da06fd0ddd9ed0d4bb3")]
 [assembly: System.Reflection.AssemblyProductAttribute("myapp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("myapp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
